5631d6b07df7dc520a8774841d95f8a6fce7cae5
Sahabudin Ali <52666069+sahabudinali@users.noreply.github.com>
2022-07-25 22:53:41 +0530

    Update BinaryTreeDepth.java

--- BinaryTree/BinaryTreeDepth.java
+++ BinaryTree/BinaryTreeDepth.java
@@ -10,7 +10,7 @@ class Node
 	}
 }
 class Binary {
-
+        // purpose of the Node root is to define root, if we are not mention the Node root then it say can't find the symbol of root which we are sending the data.
 	Node root;
 	int maxDepth(Node node)
 	{
@@ -27,6 +27,12 @@ class Binary {
 				return (lDepth + 1);
 			else
 				return (rDepth + 1);
+			
+			// this statement take little bit as compare to max function because first it store the value and then compare the values and in math function
+			// only it time for recursion call every time
+			// both the solution hvae big O(n) time complexity.
+			// Math.max(maxDepth(node.left),maxDepth(node.right))+1; here we are basically count only the  
+			//ages and the last we add the 1 becuase we didn't the count the root node that's we are adding one into it.
 		}
 	}
 	public static void main(String[] args)
